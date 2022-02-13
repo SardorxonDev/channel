8c3911de13a2593f42dcfeb415db4fa87cdeabf4
SardorxonDev <99531265+SardorxonDev@users.noreply.github.com>
2022-02-13 05:33:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -31,3 +31,48 @@ dp.register_message_handler(cheking, text='Проверить')
 
 if __name__ == '__main__':
     executor.start_polling(dp)
+import logging
+
+import wikipedia
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '5132672804:AAEKdZZktEGWrqHCjEA6cbC26h_pTM2XO5U'
+
+wikipedia.set_lang('uz')
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+
+async def send_welcome(message: types.Message):
+
+    """
+
+    Assalomu alaykum 
+
+    """
+
+    await message.reply("Wikipeida Botiga Xush Kelibsiz!")
+
+@dp.message_handler()
+
+async def sendWiki(message: types.Message):
+
+    try:
+
+        respond = summary(message.text)
+
+        await message.answer(respond)
+
+    except:
+
+        await message.answer("Bu mavzuga oid maqola topilmadi")
+
+if __name__ == '__main__':
+
+    executor.start_polling(dp, skip_updates=True)
