338cdc1d04e1014e51fc01a46fb0d44d2c51fcc6
SardorxonDev <99531265+SardorxonDev@users.noreply.github.com>
2022-02-13 19:33:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-import telebot
-import requests
-import vodka
-Token = "2048104428:AAGhJ8XMLRui-NS6RM24ygMIgmduBVf1YeI"
-bot = telebot.TeleBot(Token)
-
+import telebot , requests
+z = '5048688041:AAHM2SXd7rmLn6nO5cX7rfllDB8XkpnKM8c'
+bot = telebot.TeleBot(z)
 @bot.message_handler(commands=['start'])
-def buto(message):
-    bot.send_message(message.chat.id,f'<b>Hi VODKA #1st - python☬\n- - - - - - - - - - - \nWelcome To English Decoration Bot!\nSend Your Name To Decorate!\n- - - - - - - - - - - \nBY : @Vodka_tk</b>', parse_mode='html')
-
-
-@bot.message_handler(func=lambda m:True)
-def get(message):
-    bot.send_message(message.chat.id,f'<b>- Wait</b>', parse_mode='html')
-    msg = message.text
-    url = requests.get('https://brok-aapi.ml/API/En.php?en='+msg).text
-    bot.send_message(message.chat.id,f'{url}', parse_mode='markdown')
-
-pass
-bot.polling(True)
-
+def jhgf(message):
+    bot.send_message(message.chat.id,'tik tokdan video link tashlang: *',parse_mode='markdown')
+@bot.message_handler(func=lambda m: True)
+def ka(message):
+    m = message.text
+    bot.send_message(message.chat.id,'* Wait ..*',parse_mode='markdown')
+    u = requests.get(f'https://kabospythonanywhere.nhnh7.repl.co/api/tik?url={m}').json()['url']
+    bot.send_video(message.chat.id,u,caption='* Done Download Video :*',parse_mode='markdown')
+bot.polling()
