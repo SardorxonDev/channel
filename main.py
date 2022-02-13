d372e2089c029dc801c5e1244cb85811122172f2
SardorxonDev <99531265+SardorxonDev@users.noreply.github.com>
2022-02-13 19:53:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-import telebot , requests
-z = '5048688041:AAHM2SXd7rmLn6nO5cX7rfllDB8XkpnKM8c'
-bot = telebot.TeleBot(z)
+import telebot
+from telebot import types
+import requests
+bot = telebot.TeleBot("5048688041:AAHnrcDEsGTBr-PLQmoEd2AG92lcIpyE9_Y")
 @bot.message_handler(commands=['start'])
-def jhgf(message):
-    bot.send_message(message.chat.id,'tik tokdan video link tashlang: *',parse_mode='markdown')
-@bot.message_handler(func=lambda m: True)
-def ka(message):
-    m = message.text
-    bot.send_message(message.chat.id,'* Wait ..*',parse_mode='markdown')
-    u = requests.get(f'https://kabospythonanywhere.nhnh7.repl.co/api/tik?url={m}').json()['url']
-    bot.send_video(message.chat.id,u,caption='* Done Download Video :*',parse_mode='markdown')
-bot.polling()
+def first_msg(message):
+    maac = types.InlineKeyboardMarkup()
+    vodka = types.InlineKeyboardButton(text='Coder',url='https://t.me/Vodka_Tools')
+    maac.row_width = 2
+    maac.add(vodka)
+    bot.send_message(message.chat.id,text=f'*Hi {message.from_user.first_name}\n- - - - - - - -\nWelcome To Instagram Video Downloader\nSend Video Link To Download It\n- - - - - - - -\nBY : @Vodka_Tools*',parse_mode='markdown',reply_to_message_id=message.message_id,reply_markup=maac)
+@bot.message_handler(func=lambda m:True)
+def url_message(message):
+    msg = message.text
+    url = requests.get(f'https://mr-abood.herokuapp.com/Get/Video/Insta?Link={msg}').json()['link']
+    text = '*Here Is Your Video\n- - - - - - - -\nBY : @Vodka_Tools*'
+    bot.send_video(message.chat.id,url,caption=text,parse_mode='markdown')
+bot.polling(True)
