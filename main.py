d2e7c0ebab3b27ecf973bfad574f4a2722a12f21
SardorxonDev <99531265+SardorxonDev@users.noreply.github.com>
2022-02-13 23:43:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import requests
 import telebot
 
-token = "5205831766:AAEndNC2J8Kg_V_VEYBn5regsE1A9PfA2DU"
+token = "1380225669:AAGg5Ya19f9NASqPNjht4UPALo4AVFs1a48"
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(message.chat.id,f"<strong>Hi,\n== === ==\nWellcome Bro Send Number From 1 To 100 To Get Quran Sora.\n== === ==\nBy : @trprogram </strong>",parse_mode="html")
+    bot.send_message(message.chat.id,f"<strong>Assalom alaykum va raxmatullohu va barokotuhu,\n== === ==\nQur'ondan sura olish uchun 1 dan 116 gacha raqam yuboring.\n== === ==\n : @run_2022 </strong>",parse_mode="html")
 @bot.message_handler(func=lambda m:True)
 def so(message):
-    bot.send_message(message.chat.id,f"<strong>Wait.. </strong>",parse_mode="html")
+    bot.send_message(message.chat.id,f"<strong>Iltimos kuting.. </strong>",parse_mode="html")
     url = requests.get(f"https://timoa.ml/API/quran.php?n={message.text}&type=url").json()["url"]
     bot.send_photo(message.chat.id,url,caption=f"<strong> Quran Sora\nNumber : {message.text}</strong>",parse_mode="html")
 bot.polling() 
