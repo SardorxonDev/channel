6f27f7d009108b854b15a74dd3ed05ae55b22a73
SardorxonDev <99531265+SardorxonDev@users.noreply.github.com>
2022-02-13 20:06:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-import telebot
-from telebot import types
 import requests
+import os
+import telebot
+
 bot = telebot.TeleBot("5048688041:AAHnrcDEsGTBr-PLQmoEd2AG92lcIpyE9_Y")
-@bot.message_handler(commands=['start'])
-def first_msg(message):
-    maac = types.InlineKeyboardMarkup()
-    vodka = types.InlineKeyboardButton(text='Coder',url='https://t.me/Vodka_Tools')
-    maac.row_width = 2
-    maac.add(vodka)
-    bot.send_message(message.chat.id,text=f'*Hi {message.from_user.first_name}\n- - - - - - - -\nWelcome To Instagram Video Downloader\nSend Video Link To Download It\n- - - - - - - -\nBY : @Vodka_Tools*',parse_mode='markdown',reply_to_message_id=message.message_id,reply_markup=maac)
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id,f"Hi\n===========\nWellcome To YouTube Download Bot\nSend Link  Now\n===========\nBy : @trprogram")
 @bot.message_handler(func=lambda m:True)
-def url_message(message):
-    msg = message.text
-    url = requests.get(f'https://mr-abood.herokuapp.com/Get/Video/Insta?Link={msg}').json()['link']
-    text = '*Here Is Your Video\n- - - - - - - -\nBY : @Vodka_Tools*'
-    bot.send_video(message.chat.id,url,caption=text,parse_mode='markdown')
-bot.polling(True)
+def send(message):
+    url = requests.get(f"https://timoa.ml/API/youtube.php?url={message.text}").json()
+    y = url["result"]
+    bot.send_video(message.chat.id,y,caption="Done")
+bot.polling()
